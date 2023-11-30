405fa0cf6e850351d66217220d61e335e38ed2b8
Michael Rainer <m.rainer@kreatif.it>
2023-11-30 11:56:42 +0100

    fixes

--- lib/AssioPay.php
+++ lib/AssioPay.php
@@ -3,6 +3,7 @@
 namespace AssioPay;
 
 use GuzzleHttp\Client;
+use rex_config;
 
 class AssioPay
 {
@@ -15,24 +16,51 @@ class AssioPay
         $this->doLogin();
     }
 
-    protected function doLogin(): bool
+    protected function doLogin(): void
     {
-        $this->client = new Client(
+        $isSandbox = $this->getSettingValue('assiopay_use_sandbox');
+
+        if ($isSandbox) {
+            $mail = $this->getSettingValue('assiopay_sandbox_mail');
+            $password = $this->getSettingValue('assiopay_sandbox_password');
+        } else {
+            $mail = $this->getSettingValue('assiopay_live_mail');
+            $password = $this->getSettingValue('assiopay_live_password');
+        }
+        $credentials = [
+            'email' => $mail,
+            'password' => $password,
+        ];
+
+        /*$this->client = new Client(
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
                 ],
             ]
-        );
+        );*/
+    }
+
+    public function getSettingValue($key)
+    {
+        return rex_config::get('assiopay', $key);
     }
 
     /**
      * @param string $cardHash
      * @return array
      */
-    public function getCardInfoForCardHash(string $cardHash): array
+    public function getCardInfoForCardHash(string $cardHash): ?array
     {
-        return [];
+        return null;
+        return [
+            'hash' => 'dasf345',
+            'balance' => '500',
+            'value' => '10',
+            'fractionedTicketValue' => '30',
+            'expiry' => '2024-06-06',
+            'nofWholeTickets' => '3jk4df435',
+        ];
     }
 }
 
